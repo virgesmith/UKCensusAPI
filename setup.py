46080181045201524e4b55d5ca2a0dcd2c574c17
virgesmith <a.p.smith@leeds.ac.uk>
2017-08-08 16:48:01 +0100

    using python package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ukcensusapi',
+      version='0.1',
+      description='UK census data query automation',
+      url='https://github.com/virgesmith/UKCensusAPI',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=['ukcensusapi'],
+      zip_safe=False)
