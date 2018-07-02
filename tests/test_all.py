aab1018ca284d2e4dc881aff8917ed99331d8f61
virgesmith <a.p.smith@leeds.ac.uk>
2018-07-02 22:23:51 +0100

    looks like nomisweb have changed the MYE data (why can't they add a new table???)

--- tests/test_all.py
+++ tests/test_all.py
@@ -194,7 +194,7 @@ class Test(TestCase):
 
     table = self.api_ew.get_data(table_internal, query_params)
     self.assertEqual(table.shape, (364, 4))
-    self.assertEqual(sum(table.OBS_VALUE), 591572)
+    self.assertEqual(sum(table.OBS_VALUE), 592012)
 
   def test_get_and_add_descriptive_column(self):
 
